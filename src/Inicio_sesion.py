551b5d9d94fa16b50ce8f9ba0e8982bd6946595b
AC-Santiago <acostasantiago311124578@gmail.com>
2023-04-01 19:23:38 -0500

    Se agrgo la interfaz del programa y se agrego configuraciuón de el visual en el proyecto

--- src/Inicio_sesion.py
+++ src/Inicio_sesion.py
@@ -1,5 +1,65 @@
-class usuario():
-    pass
-    def __init__(self, user, password):
-        self.user = user
-        self.password = password
+#Crea una entrada de texto que puede detectar la entrada de caracteres especiales en una interfa de usuario
+#con la libreria PyQT5
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QRegExpValidator
+import sys
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(400, 300)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(20, 20, 81, 16))
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(20, 40, 113, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(20, 70, 81, 16))
+        self.label_2.setObjectName("label_2")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(20, 90, 113, 20))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 120, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.ingresar)
+        self.pushButton.clicked.connect(MainWindow.close)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(20, 150, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.clicked.connect(self.salir)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Usuario"))
+        self.label_2.setText(_translate("MainWindow", "Contraseña"))
+        self.pushButton.setText(_translate("MainWindow", "Ingresar"))
+        self.pushButton_2.setText(_translate("MainWindow", "Salir"))
+
+    def ingresar(self):
+        if self.lineEdit.text() == "admin" and self.lineEdit_2.text() == "1234":
+            QMessageBox.about(self, "Mensaje", "Bienvenido")
+        else:
+            QMessageBox.about(self, "Mensaje", "Usuario o contraseña incorrectos")
+
+    def salir(self):
+        sys.exit()
+
+Ui_MainWindow()
+
+
+
+
+
+
