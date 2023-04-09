86c72ea0a2ca30c57a610beb51ccc0f6aba396ba
AC-Santiago <acostasantiago311124578@gmail.com>
2023-04-08 19:07:43 -0500

    Se agrego la pestaña de inicio de sesion con una carpeta en donde se contiene las imagenbes y un archivo que contiene los estilos de los elementos

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,11 @@
-from Inicio_sesion import Ui_Inicio_sesion
+from Inicio_sesion import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Inicio_sesion = QtWidgets.QMainWindow()
-    ui = Ui_Inicio_sesion()
-    ui.setupUi(Inicio_sesion)
-    Inicio_sesion.show()
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
