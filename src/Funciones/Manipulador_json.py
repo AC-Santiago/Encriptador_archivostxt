5dade3c09f56553560681e7b1f4191c50fd5659c
AC-Santiago <acostasantiago311124578@gmail.com>
2023-05-06 19:18:08 -0500

    Se agregaron funciones nuevas al Manipulador_json y se agrego un json que sera el que guarde el nombre del usuario y si este tiene la opcion de mantener la sesion mantenida

--- src/Funciones/Manipulador_json.py
+++ src/Funciones/Manipulador_json.py
@@ -13,14 +13,14 @@ class manage_json:
     # Funcion que obtenga el json y la meta en una variable para poder manipularla
     def get_json(self):
         try:
-            with open(self.ruta) as contenido:
+            with open(self.ruta, "r") as contenido:
                 Datos = json.load(contenido)
                 if type(Datos) == list:  # Si es una lista y diccionario
-                    print("Es una lista y diccionario")
+                    # print("Es una lista y diccionario")
                     self.tipo = 1
                     self.lent = len(Datos)
                 elif type(Datos) == dict:  # Si es un diccionario
-                    print("Es un diccionario")
+                    # print("Es un diccionario")
                     self.tipo = 2
                     self.lent = len(Datos)
             return Datos
@@ -38,20 +38,51 @@ class manage_json:
                 for Dato in Datos:  # type:ignore
                     lista_resultado.append(Dato.get(elemento))
                     contador += 1
-                print(contador)
+                # print(contador)
                 return lista_resultado
 
             elif self.tipo == 2:
                 resultado = list()
                 Datos = self.get_json()
                 resultado.append(Datos.get(elemento))  # type:ignore
+                return resultado
+
+    # Funcion que pueda editar valores del json y guardarlos en el archivo
+    def edit_element(self, elemento, valor1, valor2):
+        if self.errorfoundfield == 0:
+            contador = 0
+            if self.tipo == 1:
+                with open(self.ruta, "r") as f:
+                    data = json.load(f)
+
+                for diccionario in data:
+                    if diccionario[elemento] == valor1:
+                        diccionario[elemento] = valor2
+                        contador += 1
+
+                # Guardar los cambios en el archivo JSON
+                with open(self.ruta, "w") as f:
+                    json.dump(data, f)
+
+            elif self.tipo == 2:
+                with open(self.ruta, "r") as f:
+                    data = json.load(f)
+
+                if data[elemento] == valor1:
+                    data[elemento] = valor2
+                    contador += 1
+
+                # Guardar los cambios en el archivo JSON
+                with open(self.ruta, "w") as f:
+                    json.dump(data, f)
+
+            if contador == 0:
+                print("No se encontro el elemento")
 
 
 if __name__ == "__main__":
     ruta = "./src/Archivos.json/Nombre_usuario.json"
     prueba = manage_json(ruta)
     print(prueba.get_json())
-    # print(prueba.tipo)
-    # print(prueba.lent)
-    resultado = prueba.get_element("Estado_check")
-    print(resultado)
+    prueba.edit_element("Nombre", "Santiago", "SantiagoA")
+    print(prueba.get_json())
