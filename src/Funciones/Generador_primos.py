587a9eabd5d414423700a754d232798c38be23f6
AC-Santiago <acostasantiago311124578@gmail.com>
2023-06-27 19:58:12 -0500

    Se realizo un dicciionario con números primos los cuales el programa escogera de manera aleatoria estos números estan en el rango de 200 a 1000, estos fueron generados por una función

---
+++ src/Funciones/Generador_primos.py
@@ -0,0 +1,32 @@
+from Manipulador_json import manage_json
+
+
+#! Verifica que los numeros sean primos
+def primo_check(a):
+    contador = int(0)
+    for i in range(1, a + 1):
+        residuo = int(a % i)
+        if residuo == 0:
+            contador += 1
+        elif contador > 2:
+            break
+    if contador == 2:
+        return True
+    else:
+        return False
+
+
+def Gene_pri():
+    path = "src/Archivos.json"
+    Archivo = manage_json(path)
+    lista = list()
+    for i in range(100, 1000):
+        if primo_check(i):
+            lista.append(i)
+    Archivo.create_json(
+        path, "Numeros_primos.json", '{"Numeros_primos": ' + str(lista) + "}"
+    )
+
+
+if __name__ == "__main__":
+    Gene_pri()
