df47afb5da2ccffc746c86d8fd92d12bd6f361e1
AC-Santiago <acostasantiago311124578@gmail.com>
2023-09-18 17:22:13 -0500

    Se creo un app distintan con el proposito de manejo de usuarios en el cual tiene dos views y un modelo

--- EncrytedPage/settings.py
+++ EncrytedPage/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "Dec_Cif",
+    "Users_EncrytedPage",
 ]
 
 MIDDLEWARE = [
---
+++ Users_EncrytedPage/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def Login(request):
+    return HttpResponse("<h1>Login Page</h1>")
+
+
+def Register(request):
+    return HttpResponse("<h1>Register Page</h1>")
