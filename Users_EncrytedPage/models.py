d3b5b5abd9ea71aed9f52ba3dd467b98f7798960
AC-Santiago <acostasantiago311124578@gmail.com>
2023-09-18 22:04:35 -0500

    Se creao la tabla de usuario

--- Users_EncrytedPage/models.py
+++ Users_EncrytedPage/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 # Create your models here.
-class Users(models.Model):
-    username = models.CharField(max_length=100)
+class users(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
--- Users_EncrytedPage/views.py
+++ Users_EncrytedPage/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
-def Login(request):
+
+
+def login(request):
     return HttpResponse("<h1>Login Page</h1>")
 
 
-def Register(request):
+def register(request):
     return HttpResponse("<h1>Register Page</h1>")
