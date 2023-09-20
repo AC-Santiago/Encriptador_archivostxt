00b4030fedbb136ae80d6f2f0772db15130b1607
AC-Santiago <acostasantiago311124578@gmail.com>
2023-09-20 16:57:04 -0500

    Se agregaron las templates de login y register

--- Users_EncrytedPage/views.py
+++ Users_EncrytedPage/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 
 def login(request):
-    return HttpResponse("<h1>Login Page</h1>")
+    name_user = str("santiago")
+    return render(request, "Login_U.html", {"name_user": name_user})
 
 
 def register(request):
