dec07324532f89e263471e1a99650fab8be41fc8
AC-Santiago <acostasantiago311124578@gmail.com>
2023-09-21 18:25:50 -0500

    Se crearon las nuevas tablas las cuales son las de llaves y la de contraseñas

--- Users_EncrytedPage/models.py
+++ Users_EncrytedPage/models.py
@@ -9,3 +9,23 @@ class users(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class keys(models.Model):
+    public_key = models.CharField(max_length=200)
+    private_key = models.CharField(max_length=200)
+    user = models.ForeignKey(users, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class passwords(models.Model):
+    origen = models.CharField(max_length=200)
+    user_name = models.CharField(max_length=200)
+    correo_electronico = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    user = models.ForeignKey(users, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
