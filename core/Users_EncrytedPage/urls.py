f06a5a273eaa776f4c676ad88d0a5acbc3adaa40
AC-Santiago <acostasantiago311124578@gmail.com>
2023-09-30 11:44:11 -0500

    Se creo la funcionalidad de agregar ususarios y poder loguearse en ella

--- core/Users_EncrytedPage/urls.py
+++ core/Users_EncrytedPage/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path("register/", views.register_page, name="register"),
+    path("login/", views.login_page, name="login"),
+    path("logout/", views.sign_out, name="logout"),
 ]
--- core/Users_EncrytedPage/views.py
+++ core/Users_EncrytedPage/views.py
@@ -1,9 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from .models import Users_EncrytedPage as Users
+
 
 # Create your views here.
-def register(request):
-    return render(request, "Register_U.html")
+def register_page(request):
+    if request.method == "POST":
+        if request.POST["password"] != request.POST["password2"]:
+            return render(
+                request, "Register_U.html", {"error": "Passwords do not match"}
+            )
+        else:
+            try:
+                user = Users.objects.create_user(
+                    username=request.POST["username"],
+                    email=request.POST["email"],
+                    password=request.POST["password"],
+                )
+                user.save()
+                login(request, user)
+                return redirect("login")
+            except IntegrityError:
+                return render(
+                    request, "Register_U.html", {"error": "Username already taken"}
+                )
 
-def login(request):
-    name_user = "Toby2003"
-    return render(request, "Login_U.html", {"name_user": name_user})
+    elif request.method == "GET":
+        return render(request, "Register_U.html")
+
+
+def login_page(request):
+    if request.method == "POST":
+        user = authenticate(
+            request,
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
+        if user is None:
+            return render(
+                request,
+                "Login_U.html",
+                {
+                    "error": "Username and password did not match",
+                    "form": AuthenticationForm,
+                },
+            )
+        else:
+            login(request, user)
+            return redirect("home")
+    elif request.method == "GET":
+        return render(request, "Login_U.html", {"form": AuthenticationForm})
+
+
+def sign_out(request):
+    logout(request)
+    return redirect("login")
