e4b404d7ef85e133de63640369ed591d0d70fe78
AC-Santiago <acostasantiago311124578@gmail.com>
2023-10-12 22:46:15 -0500

    Se modificó las clases de los widget del formulario de creación de contraseñas y con ellos también la de editar contraseñas.

--- core/Passwords_Users/forms.py
+++ core/Passwords_Users/forms.py
@@ -1,8 +1,8 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Passwords_users
 
 
-class PasswordsUsersForm(ModelForm):
+class PasswordsUsersForm(forms.ModelForm):
     class Meta:
         model = Passwords_users
         fields = [
@@ -12,6 +12,53 @@ class PasswordsUsersForm(ModelForm):
             "password_origin",
             "password",
         ]
+        widgets = {
+            "password": forms.PasswordInput(
+                attrs={
+                    "class": "form-control",
+                    "type": "password",
+                    "placeholder": "Contraseña",
+                    "required": "true",
+                    "id": "floatingInputPassword",
+                }
+            ),
+            "password_name": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Nombre",
+                    "required": "true",
+                    "id": "floatingInputName",
+                    "type": "text",
+                }
+            ),
+            "password_username": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Usuario",
+                    "type": "text",
+                    "required": "true",
+                    "id": "floatingInputUsername",
+                }
+            ),
+            "password_email": forms.EmailInput(
+                attrs={
+                    "type": "email",
+                    "class": "form-control",
+                    "id": "floatingInputEmail",
+                    "required": "true",
+                    "placeholder": "",
+                }
+            ),
+            "password_origin": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Origen",
+                    "type": "text",
+                    "required": "true",
+                    "id": "floatingInputOrigin",
+                }
+            ),
+        }
 
     # Desactiva el aoutocompletado de los campos del formulario
     def __init__(self, *args, **kwargs):
